b9e0b8ff5c36139e54f3d644dfbde5e983d0d1f2
Emma Romero <emmayrs17@gmail.com>
2020-07-26 20:18:15 -0500

    esquema del proyecto

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import web
 
 urls = (
     '/', 'mvc.controllers.index.Index',
+    '/list', 'mvc.controllers.actions.list.Lista',
+    '/insert', 'mvc.controllers.actions.insert.Insert',
+    '/delete', 'mvc.controllers.actions.delete.Delete',
+    '/view', 'mvc.controllers.actions.viewOne.View',
+    '/update', 'mvc.controllers.actions.update.Update',
 )
 app = web.application(urls, globals())
 
